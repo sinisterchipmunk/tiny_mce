39403af28ddba6d9763c78faec39ab315a2987ee
sinisterchipmunk <sinisterchipmunk@gmail.com>
2010-02-25 22:18:58 -0500

    Added a "tiny_mce_init" method so you can initialize multiple times (if that's necessary...)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
   def tiny_mce(options = {})
+    r = (@tiny_mce_included ? '' : javascript_include_tag("tiny_mce/tiny_mce")) + "\n" +
+            #javascript_tag("tinyMCE_GZ.init(#{gz.to_json})") + "\n" + # compressor for performance
+            javascript_tag(tiny_mce_init(options))
+    @tiny_mce_included ||= true
+    r
+  end
+
+  def tiny_mce_init(options = {})
     config = TinyMce.configuration.merge(options)
 #    gz = {
 #      :themes => config[:theme],
@@ -15,11 +23,7 @@ module ApplicationHelper
     buttons.length.times do |i|
       mce["theme_advanced_buttons#{i+1}"] = buttons[i].join(',') unless mce["theme_advanced_buttons#{i+1}"]
     end if buttons
-    
-    r = (@tiny_mce_included ? '' : javascript_include_tag("tiny_mce/tiny_mce")) + "\n" +
-            #javascript_tag("tinyMCE_GZ.init(#{gz.to_json})") + "\n" + # compressor for performance
-            javascript_tag("tinyMCE.init(#{mce.to_json});")
-    @tiny_mce_included ||= true
-    r
+
+    "tinyMCE.init(#{mce.to_json});"
   end
 end
